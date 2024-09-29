4c9ca7a99e48320a6976111168acec472cfcc206
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-29 13:56:47 +0530

    added libraries and interfaces

---
+++ src/interfaces/Article.ts
@@ -0,0 +1,15 @@
+import {IComment} from "./Comment"
+import { ITag } from "./Tag";
+
+export interface IArticle {
+    _id: string;
+    author:{
+        username: string;
+        _id: string;
+    };
+    title: string;
+    content: string;
+    timestamp: Date;
+    tags: ITag[];
+    comments: IComment[];
+}
---
+++ src/interfaces/customTypes.ts
@@ -0,0 +1,3 @@
+export type ViewType = "All" | "Latest" | "About" | "Other";
+
+export type ThemeType = 'Default' | "Matter" | "Dark" | "Bright" | "Chameleon" ;
