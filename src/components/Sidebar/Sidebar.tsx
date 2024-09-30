178c7600f8e2224503fdc9acff36e454ad9f6e06
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-30 16:42:43 +0530

    tag section

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
 import "./Sidebar.scss"
 import SocialLinks from "./SocialLinks/SocialLinks";
+import TagsSection from "./TagsSection/TagsSection";
 import ThemeSection from './ThemeSection/ThemeSection';
 
 export default function Sidebar () {
     return (
         <div className = "sidebar">
+            <div className="sidebar-section">
+                <TagsSection/>
+            </div>
             <div className ="sidebar-theme-switch">
                 <ThemeSection aria-label="Toggle Theme"/>
             </div>
