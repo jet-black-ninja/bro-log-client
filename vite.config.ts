af088566bb41f156bab6f014bc07d1116147d672
jet_black_ninja <pramodkmr958@gmail.com>
2024-10-27 18:25:50 +0530

    deployment woes 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+if (!process.env.VITE_SERVER_URL) {
+  throw new Error('VITE_SERVER_URL is not defined in the environment variables');
+}
 export default defineConfig({
   server: {
     proxy: {
@@ -8,6 +11,7 @@ export default defineConfig({
         target: process.env.VITE_SERVER_URL,
         changeOrigin: true, // Ensures the host header matches the target URL
         secure: false, // Disable if your server is using an insecure connection (http instead of https)
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
